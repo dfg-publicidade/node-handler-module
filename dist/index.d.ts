074d84f4b24a53a7cb39d3a37162c4268ccfdb14
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2021-07-13 15:20:02 -0300

    Adição do handler para upload

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,7 @@
 import ErrorHandler from './handlers/errorHandler';
 import InvalidRequestHandler from './handlers/invalidRequestHandler';
+import InvalidUploadHandler from './handlers/invalidUploadHandler';
 import NotFoundHandler from './handlers/notFoundHandler';
 import ServerErrorHandler from './handlers/serverErrorHandler';
 import SuccessHandler from './handlers/successHandler';
-export { ErrorHandler, InvalidRequestHandler, NotFoundHandler, ServerErrorHandler, SuccessHandler };
+export { ErrorHandler, InvalidRequestHandler, InvalidUploadHandler, NotFoundHandler, ServerErrorHandler, SuccessHandler };
--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SuccessHandler = exports.ServerErrorHandler = exports.NotFoundHandler = exports.InvalidRequestHandler = exports.ErrorHandler = void 0;
+exports.SuccessHandler = exports.ServerErrorHandler = exports.NotFoundHandler = exports.InvalidUploadHandler = exports.InvalidRequestHandler = exports.ErrorHandler = void 0;
 const errorHandler_1 = __importDefault(require("./handlers/errorHandler"));
 exports.ErrorHandler = errorHandler_1.default;
 const invalidRequestHandler_1 = __importDefault(require("./handlers/invalidRequestHandler"));
 exports.InvalidRequestHandler = invalidRequestHandler_1.default;
+const invalidUploadHandler_1 = __importDefault(require("./handlers/invalidUploadHandler"));
+exports.InvalidUploadHandler = invalidUploadHandler_1.default;
 const notFoundHandler_1 = __importDefault(require("./handlers/notFoundHandler"));
 exports.NotFoundHandler = notFoundHandler_1.default;
 const serverErrorHandler_1 = __importDefault(require("./handlers/serverErrorHandler"));
